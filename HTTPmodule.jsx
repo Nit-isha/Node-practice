39bd5e0304c1469529a21b1dc82f798326e4dafd
Nitisha <nitishagupta.1999@gmail.com>
2021-12-23 09:48:16 +0530

    feat HTTP

---
+++ HTTPmodule.jsx
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  //request,response
+  if (req.url === "/") {
+    res.end("Welcome to home page");
+  }
+  if (req.url === "/about") {
+    res.end("About page");
+  }
+  res.end(`
+  <h1> Ooops! </h1>
+  <p> We can't seem to find page. </p>
+  <a href = "/"> Back Home </a>
+  `);
+});
+
+server.listen(5000);
+//localhost:5000
