90e7dcef1306ee8bb93b13cdfa5b033e404dc1c0
Nitisha <nitishagupta.1999@gmail.com>
2021-12-23 10:09:06 +0530

    feat: gitignore and package installed

--- app.js
+++ app.js
@@ -7,3 +7,23 @@
 // process - info about env where the program is being executed.
 
 //Built in modules : OS, PATH, FS, HTTP
+
+//## NPM
+//npm - global command, comes with node
+//npm --version
+
+//local dependency - use it in any object
+//npm i <packageName>
+
+//global dependency - use it in any project
+//npm install -g <packageName>
+
+//package.json - manifest files(stores imp info about project).
+//manual approach ( create package.json in the root, create properties)
+//npm init (step-by-spet)
+//npm init -y (everything default)
+//e.g. package lodash
+// const _ = require("lodash");
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
