2162d286f219bba4f92f2a70897ef7f030e8a186
Nitisha <nitishagupta.1999@gmail.com>
2021-12-24 10:52:32 +0530

    feat: event HTTP

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   res.end("Welcome");
-// });
-
-//using Event Emitter API
-const server = http.createServer();
-
-//emits request event
-
-server.on("request", (req, res) => {
-  res.end("Welcome");
-});
-
-server.listen(5000);
